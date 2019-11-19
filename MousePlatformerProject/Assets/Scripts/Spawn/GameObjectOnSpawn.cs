11acae6c0ab7bb833df2b5f72c188f2c6f662eab
blizzard190 <22256@ma-web.nl>
2019-11-19 14:55:38 +0100

    Made GameObjectOnSpawn and PlayerSwitch script
    Made the object follow the mouse until mouseButton 0 is clicked. After which the object takes the posistion of the player and becomes the player.

---
+++ MousePlatformerProject/Assets/Scripts/Spawn/GameObjectOnSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectOnSpawn : MonoBehaviour
+{
+    void Start()
+    {
+        //Get the mouse posistion
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepos.z = 0;
+        //Make the objects postion = to the mouse position
+        transform.position = mousepos;
+    }
+
+    void Update()
+    {
+        //Get the mouse posistion
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepos.z = 0;
+        //Make the objects postion = to the mouse position
+        transform.position = mousepos;
+    }
+}
